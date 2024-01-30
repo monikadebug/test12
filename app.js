d7a74076515fd114b3fdb60bea4cdca13f0b22ee
Monika <monika.jsx@gmail.com>
2024-01-30 21:59:41 +0530

    update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let body=document.getElementById("main")
 let btn=document.getElementById("btn")
 let text=document.getElementById("code")
 
-
+const arr=[0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f"]
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -11,13 +11,17 @@ function getRandomInt(min, max) {
   }
 
 function changeBG(){
-    console.log("hiii");
-    const code = `#${getRandomInt(1,9)}${getRandomInt(1,9)}${getRandomInt(1,9)}${getRandomInt(1,9)}${getRandomInt(1,9)}${getRandomInt(1,9)}`
+  const code =`#${arr[getRandomInt(0,15)]}${arr[getRandomInt(0,15)]}${arr[getRandomInt(0,15)]}${arr[getRandomInt(0,15)]}${arr[getRandomInt(0,15)]}${arr[getRandomInt(0,15)]}`
+  
+  console.log(code);
+
     main.style.backgroundColor= code;
     text.innerHTML=code
 }
 
 btn.addEventListener("click", changeBG)
+document.addEventListener("click", changeBG)
+
 
 
 
